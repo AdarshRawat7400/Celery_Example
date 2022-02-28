33e9928146a885983808ed96d94f984cb74de37d
Adarsh Rawat <56146385+AdarshRawat7400@users.noreply.github.com>
2022-02-28 22:58:43 +0530

    Update settings.py

--- celerytask (Django Implementation)/celerytask/settings.py
+++ celerytask (Django Implementation)/celerytask/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'adarshrawat71@gmail.com'
-EMAIL_HOST_PASSWORD = 'ixuhcoulrlciqtop'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'AdcoreRaven Team <noreply@adcoreraven.com>'
